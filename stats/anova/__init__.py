b976507a9bd50b3ed03c9c7167d74e13867b7ca5
Aravind Sundararajan <Aravind.Sundararajan@protonmail.com>
2022-12-22 12:54:27 -0700

    switching to absolute paths for more things

---
+++ stats/anova/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2016  Todd Pataky
+
+
+from spm1d.stats.anova import designs, factors, models
+
+from spm1d.stats.anova.ui import anova1, anova1rm
+from spm1d.stats.anova.ui import anova2, anova2nested, anova2rm, anova2onerm
+from spm1d.stats.anova.ui import (
+    anova3,
+    anova3nested,
+    anova3rm,
+    anova3tworm,
+    anova3onerm,
+)
